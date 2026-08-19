cc0315fd071a1e4ef0f51ea9faeec39aadb0e3b5
booglycodes <booglycodes@gmail.com>
2026-08-19 04:50:58 +0000

    try MQTT strategy

--- network.js
+++ network.js
@@ -122,7 +122,7 @@ function getQRUrl(roomName) {
 }
 
 async function initNetwork() {
-  const { joinRoom } = await import("https://esm.run/@trystero-p2p/torrent")
+  const { joinRoom } = await import("https://esm.run/@trystero-p2p/mqtt")
 
   const roomName = Math.random().toString(36).slice(2, 12)
   room = joinRoom({ appId: TRYSTERO_APP_ID }, roomName)
